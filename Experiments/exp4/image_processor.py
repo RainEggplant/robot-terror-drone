781420c72e552b6cdd41add4fd9f22eda722b04c
RainEggplant <i@raineggplant.com>
2019-12-08 18:11:13 +0800

    增强赛道识别的精准度，增加对沟壑以及对侧赛道的识别 不再提供 brink 数据

--- Experiments/exp4/image_processor.py
+++ Experiments/exp4/image_processor.py
@@ -6,6 +6,7 @@ import numpy as np
 import cv2
 
 # %% 常数定义
+CV_VERSION = cv2.__version__.split('.')[0]  # pylint: disable=no-member
 USE_CLAHE = True
 IMG_WIDTH = 320
 IMG_HEIGHT = 240
@@ -14,22 +15,20 @@ LEFT_THRESHOLD = 50
 RIGHT_THRESHOLD = 50
 LANDMINE_SOLIDITY_THRESHOLD = 0.8
 LANDMINE_AREA_RATIO_THRESHOLD = 0.6
-BRINK_SOLIDITY_THREHOLD = 0.45
-BRINK_AREA_RATIO_THREHOLD = 0.25
-MIN_CONTOUR_AREA = {'white': 10000, 'red': 5000,
+BRINK_SOLIDITY_THRESHOLD = 0.6
+BRINK_AREA_RATIO_THRESHOLD = 0.5
+BRINK_ANGLE_DELTA = 10
+TRACK_SOLIDITY_THRESHOLD = 0.6
+TOP_LINE = int(0.01 * IMG_HEIGHT)
+BOTTOM_LINE = int(0.99 * IMG_HEIGHT)
+MIN_CONTOUR_AREA = {'white': 4000, 'red': 5000,
                     'green': 5000, 'yellow': 5000, 'black': 150}
-MIN_TRACK_BRINK_DISTANCE = -30
+MIN_TRACK_BRINK_DISTANCE = -6
 
 # 检视平面坐标点
-XT_LEFT = int(0.2 * IMG_WIDTH)
-XT_MID = int(0.5 * IMG_WIDTH)
-XT_RIGHT = int(0.8 * IMG_WIDTH)
-YT_BOTTOM = int(0.9 * IMG_HEIGHT)
-YT_MID = int(0.75 * IMG_HEIGHT)
-YT_TOP = int(0.6 * IMG_HEIGHT)
-TEST_POINTS = [(XT_LEFT, YT_BOTTOM), (XT_LEFT, YT_MID), (XT_LEFT, YT_TOP),
-               (XT_MID, YT_BOTTOM), (XT_MID, YT_MID), (XT_MID, YT_TOP),
-               (XT_RIGHT, YT_BOTTOM), (XT_RIGHT, YT_MID), (XT_RIGHT, YT_TOP)]
+XT_SCAN = np.arange(0.1 * IMG_WIDTH, IMG_WIDTH, 0.2 * IMG_WIDTH).astype(int)
+YT_TOP = int(0.01 * IMG_HEIGHT)
+YT_BOTTOM = int(0.99 * IMG_HEIGHT)
 
 # 颜色字典
 COLOR_RANGE = {
@@ -48,17 +47,13 @@ COLOR_BGR = {
     'black': (0, 0, 0)
 }
 
+
 # %% 图像处理
-
-
 class ImageProcessor(object):
     def __init__(self, stream, debug):
         self._cap = cv2.VideoCapture(stream)
         self._debug = debug
         self._disposed = False
-        # check OpenCV version
-        self._cv_version = cv2.__version__.split(  # pylint: disable=no-member
-            '.')[0]
         self.monitor = None
         # self._cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
@@ -83,40 +78,19 @@ class ImageProcessor(object):
             _, self._frame = self._cap.read()
             time.sleep(0.01)
 
-    # %% 获得轮廓面积与凸包及最小外接圆面积之比
-    def _get_area_ratio(self, contour):
-        area = cv2.contourArea(contour)
+    def _refresh_monitor(self):
+        cv2.imshow("Monitor", self.monitor)
+        cv2.waitKey(1)
 
-        # 轮廓面积与凸包的比
-        hull = cv2.convexHull(contour)
-        hull_area = cv2.contourArea(hull)
-        solidity = area / hull_area
-
-        # 轮廓面积与最小外接圆面积之比
-        (_, radius) = cv2.minEnclosingCircle(contour)
-        area_ratio = area / (math.pi * radius ** 2)
-
-        return (solidity, area_ratio)
-
-    def _determine_object_type(self, color, solidity, area_ratio):
-        if color == 'black':
-            if (solidity >= LANDMINE_SOLIDITY_THRESHOLD and
-                    area_ratio >= LANDMINE_AREA_RATIO_THRESHOLD):
-                return 'landmine'
-            if (solidity >= BRINK_SOLIDITY_THREHOLD and
-                    area_ratio <= BRINK_AREA_RATIO_THREHOLD):
-                return 'brink'
-        return 'unknown'
-
-    def _get_contours(self, img):
+    @staticmethod
+    def _preprocess_image(image):
         # 摄像头默认分辨率 640x480,
         # 处理图像时会相应的缩小图像进行处理，这样可以加快运行速度
         # 缩小时保持比例4：3, 且缩小后的分辨率应该是整数
-        img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT),
-                         interpolation=cv2.INTER_CUBIC)  # 将图片缩放
-        # self.monitor = img
-        img_gaussian = cv2.GaussianBlur(img, (3, 3), 0)  # 高斯模糊
-        img_hsv = cv2.cvtColor(img_gaussian, cv2.COLOR_BGR2HSV)
+        img_bgr = cv2.resize(image, (IMG_WIDTH, IMG_HEIGHT),
+                             interpolation=cv2.INTER_CUBIC)  # 将图片缩放
+        img_bgr = cv2.GaussianBlur(img_bgr, (3, 3), 0)  # 高斯模糊
+        img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
 
         if USE_CLAHE:
             # CLAHE
@@ -129,138 +103,259 @@ class ImageProcessor(object):
             # v = v.astype('uint8')
             clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
             v = clahe.apply(v)
-            img_clahe = cv2.merge((h, s, v))  # 合并三个通道
-            img_proc = img_clahe
-            self.monitor = cv2.cvtColor(img_clahe, cv2.COLOR_HSV2BGR)
-        else:
-            img_proc = img_hsv
-            self.monitor = img_gaussian
+            img_hsv = cv2.merge((h, s, v))  # 合并三个通道
+            img_bgr = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
 
+        return img_bgr, img_hsv
+
+    @staticmethod
+    def _get_contours(img):
         # %% 寻找颜色轮廓
         contours = {}
         for i in COLOR_RANGE:
             mask_color = cv2.inRange(
-                img_proc, COLOR_RANGE[i][0], COLOR_RANGE[i][1])  # 对原图像和掩模进行位运算
+                img, COLOR_RANGE[i][0], COLOR_RANGE[i][1])  # 对原图像和掩模进行位运算
             mask_color = cv2.morphologyEx(
                 mask_color, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))  # 开运算
             mask_color = cv2.morphologyEx(
                 mask_color, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))  # 闭运算
-            # 注意此处 opencv 2 的返回值是三元元组
 
-            if self._cv_version == '3':
+            # 注意此处 OpenCV 3 的返回值是三元元组
+            if CV_VERSION == '3':
                 (_, contours[i], _) = cv2.findContours(
-                    mask_color, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 找出轮廓
+                    mask_color, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # 找出轮廓
             else:
                 (contours[i], _) = cv2.findContours(
-                    mask_color, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 找出轮廓
+                    mask_color, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # 找出轮廓
 
-            # %% 取面积高于阈值的轮廓
+            # 取面积高于阈值的轮廓
             contours[i] = list(filter(
                 lambda x: cv2.contourArea(x) > MIN_CONTOUR_AREA[i], contours[i]))
 
         return contours
 
-    def _refresh_monitor(self):
-        cv2.imshow("Monitor", self.monitor)
-        cv2.waitKey(1)
+    def _determine_object_type(self, contour, color):
+        area = cv2.contourArea(contour)
+        # 轮廓面积与凸包的比，因为不规则图形一般是噪声
+        hull = cv2.convexHull(contour)
+        hull_area = cv2.contourArea(hull)
+        solidity = area / hull_area
+        area_ratio_circle = None
+        area_ratio_rect = None
+        angle = None
+
+        if color == 'black':
+            if solidity >= LANDMINE_SOLIDITY_THRESHOLD:
+                # 轮廓面积与最小外接圆面积之比
+                (_, radius) = cv2.minEnclosingCircle(contour)
+                area_ratio_circle = area / (math.pi * radius ** 2)
+                if area_ratio_circle >= LANDMINE_AREA_RATIO_THRESHOLD:
+                    if self._debug:
+                        print('landmine: area {}, solidity {:.3f}, area_ratio {:.3f}'.format(
+                            area, solidity, area_ratio_circle))
+                    return 'landmine'
+            if solidity >= BRINK_SOLIDITY_THRESHOLD:
+                rect = cv2.minAreaRect(contour)
+                area_ratio_rect = area / (rect[1][0] * rect[1][1])
+                angle = rect[2]
+                angle = angle + 180 if rect[1][0] < rect[1][1] else angle + 90
+                # 只选取接近水平的边缘
+                if (area_ratio_rect >= BRINK_AREA_RATIO_THRESHOLD and
+                        90 - BRINK_ANGLE_DELTA < angle < 90 + BRINK_ANGLE_DELTA):
+                    if self._debug:
+                        print('brink: area {}, solidity {:.3f}, area_ratio {:.3f}, angle {:.3f}'.format(
+                            area, solidity, area_ratio_rect, angle))
+                    return 'brink'
+
+            if self._debug:
+                prompt_unknown = 'unknown: area {}, solidity {:.3f}'.format(area, solidity)
+                if area_ratio_circle is not None:
+                    prompt_unknown = prompt_unknown + ', area_ratio_circle {:.3f}'.format(area_ratio_circle)
+                if area_ratio_rect is not None:
+                    prompt_unknown = prompt_unknown + ', area_ratio_rect {:.3f}'.format(area_ratio_rect)
+                if angle is not None:
+                    prompt_unknown = prompt_unknown + ', angle {:.3f}'.format(angle)
+                print(prompt_unknown)
+
+            return 'unknown'
+
+    def _get_tracks(self, contours, prompt=''):
+        track_cur = None
+        track_next = None
+        max_area_cur = 0
+        max_area_next = 0
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            hull = cv2.convexHull(contour)
+            hull_area = cv2.contourArea(hull)
+            solidity = area / hull_area
+            print(solidity)
+            if solidity < TRACK_SOLIDITY_THRESHOLD:
+                continue
+
+            is_cur_track = False
+            is_next_track = False
+            for xt in XT_SCAN:
+                if cv2.pointPolygonTest(contour, (xt, YT_BOTTOM), False) != -1:
+                    is_cur_track = True
+                if cv2.pointPolygonTest(contour, (xt, YT_TOP), False) != -1:
+                    is_next_track = True
+            is_next_track = is_next_track and (not is_cur_track)
+            if is_cur_track and area > max_area_cur:
+                track_cur = contour
+            if is_next_track and area > max_area_next:
+                track_next = contour
+
+            if self._debug:
+                track_type = 'current' if is_cur_track else ('next' if is_next_track else 'unknown')
+                print(prompt + 'track: area {}, {}'.format(area, track_type))
+
+        return track_cur, track_next
+
+    @staticmethod
+    def _refine_track(img, track):
+        # 标记图像中可能属于赛道的部分的位置
+        poly_track = [np.squeeze(track)]
+        mask_track = np.zeros((IMG_HEIGHT, IMG_WIDTH))
+        cv2.fillPoly(mask_track, poly_track, 1)
+        mask_track = mask_track.astype(bool)
+
+        # 取得灰度图片，求取平均亮度及标准差
+        img_track = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        max_gray = np.max(img_track[mask_track])
+        mean_gray = np.mean(img_track[mask_track])
+        std_gray = np.std(img_track[mask_track])
+
+        # 取得二值化的赛道图片
+        img_track = img_track.astype('uint8')
+        img_track[~mask_track] = 0
+        threshold = (max_gray + mean_gray) / 2 - min(std_gray * 2.6, 56)
+        _, img_bin = cv2.threshold(img_track, threshold, 255, cv2.THRESH_BINARY)
+
+        # 再提取赛道形状
+        # 注意此处 OpenCV 3 的返回值是三元元组
+        if CV_VERSION == '3':
+            (_, cnt_tracks, _) = cv2.findContours(
+                img_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # 找出轮廓
+        else:
+            (cnt_tracks, _) = cv2.findContours(
+                img_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # 找出轮廓
+
+        cnt_tracks = list(filter(
+            lambda x: cv2.contourArea(x) > MIN_CONTOUR_AREA['white'], cnt_tracks))
+        return cnt_tracks
+
+    @staticmethod
+    def _approx_track(track):
+        """ 用多边形近似平面 """
+        epsilon = 0.01 * cv2.arcLength(track, True)
+        return cv2.approxPolyDP(track, epsilon, True)
 
     def analyze_objects(self):
         current_frame = self._frame
-        contours = self._get_contours(current_frame)
-        monitor = self.monitor
+        img_bgr, img_hsv = self._preprocess_image(current_frame)
+        contours = self._get_contours(img_hsv)
+        monitor = img_bgr.copy()
         rows, cols = monitor.shape[:2]  # 图片大小
         # 画出警戒线
         monitor = cv2.line(monitor, (0, FRONT_THRESHOLD),
                            (cols - 1, FRONT_THRESHOLD), COLOR_BGR['green'], 2)
 
-        info = {}
+        info = dict()
         info['light'] = []
 
-        # 首先判断信号灯
+        # %% 识别信号灯
         keys_light = {'red', 'green', 'yellow'}
         contours_light = {key: value for key,
-                          value in contours.items() if key in keys_light}
+                                         value in contours.items() if key in keys_light}
         for i in contours_light:
-            if (len(contours_light[i]) > 0):
+            if len(contours_light[i]) > 0:
                 info['light'].append(i)
 
-        # 然后识别赛道平面
-        max_contained_points = 0
-        cnt_area = 0
-        cnt_track = None
-        for contour in contours['white']:
-            contained_points = 0
-            for point in TEST_POINTS:
-                if cv2.pointPolygonTest(contour, point, False) != -1:
-                    contained_points += 1
+        # %% 识别赛道平面
+        track_cur, track_next = self._get_tracks(contours['white'], 'possible ')
+        # 对提取的赛道进行二次处理
+        if track_cur is not None:
+            tracks = self._refine_track(img_bgr, track_cur)
+            track_cur, _ = self._get_tracks(tracks, 'candidate ')
+        if track_next is not None:
+            tracks = self._refine_track(img_bgr, track_next)
+            _, track_next = self._get_tracks(tracks, 'candidate ')
 
-            area = cv2.contourArea(contour)
-            if self._debug:
-                print('Candidate track: ', contained_points, area)
+        if track_cur is not None:
+            track_cur_approx = self._approx_track(track_cur)
+            info['track_cur'] = track_cur_approx
+            monitor = cv2.drawContours(
+                monitor, [track_cur_approx], -1, COLOR_BGR['yellow'], 3)
 
-            if (contained_points > max_contained_points or
-                    (contained_points == max_contained_points and area >= cnt_area)):
-                max_contained_points = contained_points
-                cnt_area = area
-                cnt_track = contour
+        if track_next is not None:
+            track_next_approx = self._approx_track(track_next)
+            info['track_next'] = track_next_approx
+            monitor = cv2.drawContours(
+                monitor, [track_next_approx], -1, COLOR_BGR['yellow'], 3)
 
-        # 如果无法识别到赛道平面，直接返回
-        if cnt_track is None:
-            self.monitor = monitor
-            if self._debug:
-                self._refresh_monitor()
-            return info
-
-        # 用多边形近似平面
-        epsilon = 0.01 * cv2.arcLength(cnt_track, True)
-        cnt_track_approx = cv2.approxPolyDP(cnt_track, epsilon, True)
-        info['track'] = cnt_track_approx
-        monitor = cv2.drawContours(
-            monitor, [cnt_track_approx], -1, COLOR_BGR['yellow'], 3)
-
-        # 在赛道平面上识别地雷和边缘
+        # %% 识别地雷和边缘
         info['landmine'] = []
-        info['brink'] = []
+        brink_cur = []
+        brink_next = []
         for contour in contours['black']:
-            (solidity, area_ratio) = self._get_area_ratio(contour)
-            object_type = self._determine_object_type(
-                'black', solidity, area_ratio)
-            if object_type == 'landmine':
+            object_type = self._determine_object_type(contour, 'black')
+            if track_cur is not None and object_type == 'landmine':
                 bottom_most = tuple(contour[contour[:, :, 1].argmax()][0])
                 # 只有在赛道平面内识别到的地雷才有效
-                if cv2.pointPolygonTest(cnt_track, bottom_most, False) != -1:
+                if cv2.pointPolygonTest(track_cur, bottom_most, False) != -1:
                     monitor = cv2.drawContours(
                         monitor, contour, -1, COLOR_BGR['red'], 2)
-                    # monitor = cv2.putText(monitor, str(round(
-                    #     area_ratio, 3)), bottom_most, cv2.FONT_HERSHEY_SIMPLEX, 0.4, COLOR_BGR['white'], 1)
                     info['landmine'].append(bottom_most)
-                if self._debug:
-                    print('lamdmine: ', solidity, area_ratio)
 
             elif object_type == 'brink':
-                # compute the center of the contour
-                moments = cv2.moments(contour)
-                x_c = int(moments['m10'] / moments['m00'])
-                y_c = int(moments['m01'] / moments['m00'])
-                distance = cv2.pointPolygonTest(cnt_track, (x_c, y_c), True)
-                if self._debug:
-                    print('Candidate brink: ', solidity, area_ratio, distance)
+                rect = cv2.minAreaRect(contour)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                bottom_most = tuple(box[np.argmax(box[:, 1])])
+                top_most = tuple(box[np.argmin(box[:, 1])])
 
-                # 识别到的边缘的中心必须与赛道边缘不能过远
-                if distance > MIN_TRACK_BRINK_DISTANCE:
-                    [vx, vy, x, y] = cv2.fitLine(
-                        contour, cv2.DIST_L2, 0, 0.01, 0.01)
-                    info['brink'].append((vx[0], vy[0], x[0], y[0]))
+                # 仅保留和当前和下一个赛道接近，或者接近图像上下两端的边缘
+                if track_cur is not None:
+                    distance_cur = cv2.pointPolygonTest(track_cur, bottom_most, True)
+                else:
+                    distance_cur = bottom_most[1] - (IMG_HEIGHT - 1)
 
-                    y_left = int((-x * vy / vx) + y)
-                    y_right = int(((cols - x) * vy / vx) + y)
-                    monitor = cv2.drawContours(
-                        monitor, contour, -1, COLOR_BGR['white'], 2)
-                    monitor = cv2.line(monitor, (cols - 1, y_right),
-                                       (0, y_left), COLOR_BGR['red'], 2)
-            else:
-                if self._debug:
-                    print('unknown', solidity, area_ratio)
+                if track_next is not None:
+                    distance_next = cv2.pointPolygonTest(track_next, top_most, True)
+                else:
+                    distance_next = -top_most[1]
+
+                if distance_cur > MIN_TRACK_BRINK_DISTANCE:
+                    brink_cur.append(rect)
+                elif distance_next > MIN_TRACK_BRINK_DISTANCE:
+                    brink_next.append(rect)
+
+        # %% 判断沟壑
+        if len(brink_cur) > 0 and len(brink_next) > 0:
+            rect_cur = max(brink_cur, key=lambda r: r[0][1])
+            rect_next = min(brink_next, key=lambda r: r[0][1])
+            box_cur = cv2.boxPoints(rect_cur)
+            box_next = cv2.boxPoints(rect_next)
+
+            # 提取下方矩形边界靠下的端点，转换为直线
+            order = np.argsort(box_cur[:, 1])
+            x1, y1 = box_cur[order[2]][0], box_cur[order[2]][1]
+            x2, y2 = box_cur[order[3]][0], box_cur[order[3]][1]
+            y_bt1 = int((-x2 * y1 + x1 * y2) / (x1 - x2))
+            y_bt2 = int(((IMG_WIDTH - 1 - x2) * y1 - (IMG_WIDTH - 1 - x1) * y2) / (x1 - x2))
+
+            # 提取上方矩形边界靠上的端点，转换为直线
+            order = np.argsort(box_next[:, 1])
+            x1, y1 = box_next[order[0]][0], box_next[order[0]][1]
+            x2, y2 = box_next[order[1]][0], box_next[order[1]][1]
+            y_top1 = int((-x2 * y1 + x1 * y2) / (x1 - x2))
+            y_top2 = int(((IMG_WIDTH - 1 - x2) * y1 - (IMG_WIDTH - 1 - x1) * y2) / (x1 - x2))
+
+            ditch = np.array([[IMG_WIDTH - 1, y_bt2], [0, y_bt1],
+                              [0, y_top1], [IMG_WIDTH - 1, y_top2]])
+            monitor = cv2.drawContours(monitor, [ditch], 0, (0, 0, 255), 2)
+            info['ditch'] = ditch
 
         self.monitor = monitor
         if self._debug:
@@ -268,7 +363,7 @@ class ImageProcessor(object):
 
         return info
 
-    # 终止摄像头线程，销毁对象
     def dispose(self):
+        """ 终止摄像头线程，销毁对象 """
         self._disposed = True
         self.__del__()
--- Experiments/exp4/image_processor_test.py
+++ Experiments/exp4/image_processor_test.py
@@ -2,11 +2,14 @@ import time
 from image_processor import ImageProcessor
 
 stream = "http://127.0.0.1:8080/?action=stream?dummy=param.mjpg"
+# stream = "http://192.168.137.9:8080/video"
 # stream = 0
 img_proc = ImageProcessor(stream, True)
 while 1:
     data = img_proc.analyze_objects()
-    if 'track' in data:
-        del data['track']
+    if 'track_cur' in data:
+        del data['track_cur']
+    if 'track_next' in data:
+        del data['track_next']
     print(data, '\n')
-    time.sleep(0.1)
+    time.sleep(2)
